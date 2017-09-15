d99b6cdbc50ffa04e1b302def2906c488bc53aaf
Mark Lilback <mark@lilback.com>
2017-09-15 13:16:52 -0400

    added WorkspaceIdentifier struct

--- Sources/Rc2Model/Workspace.swift
+++ Sources/Rc2Model/Workspace.swift
@@ -28,3 +28,28 @@ public struct Workspace: Codable {
 		self.dateCreated = dateCreated
 	}
 }
+
+/// struct for tracking a project/workspace combination
+public struct WorkspaceIdentifier: Codable, Hashable, CustomStringConvertible {
+	public let projectId: Int
+	public let wspaceId: Int
+	
+	public var description: String { return "wspace \(projectId)/\(wspaceId)" }
+	// read on the Interwebs that multiplying by a weird constant will prevent equal values from hashing the same
+	public var hashValue: Int { return projectId.hashValue ^ (wspaceId.hashValue &* 9_874_431) }
+	
+	public init(projectId: Int, wspaceId: Int) {
+		self.projectId = projectId
+		self.wspaceId = wspaceId
+	}
+	
+	public init?(_ wspace: Workspace?) {
+		guard let wspace = wspace else { return nil }
+		projectId = wspace.projectId
+		wspaceId = wspace.id
+	}
+	
+	public static func == (lhs: WorkspaceIdentifier, rhs: WorkspaceIdentifier) -> Bool {
+		return lhs.projectId == rhs.projectId && lhs.wspaceId == rhs.wspaceId
+	}
+}
