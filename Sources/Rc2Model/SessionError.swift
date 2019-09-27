19eda81263752c9737ec555800a1b1d104d20363
Mark Lilback <mark@lilback.com>
2019-09-27 11:57:00 -0400

    documented errors, removed unused SessionErrorCode

--- Sources/Rc2Model/SessionError.swift
+++ Sources/Rc2Model/SessionError.swift
@@ -8,20 +8,31 @@ import Foundation
 
 /// An error returned via REST or WebSocket
 public enum SessionError: String, Error, Codable, Hashable {
-	public typealias RawValue = String
-	
+	/// an unknow error happened
 	case unknown
+	/// the specified file was not found
 	case fileNotFound
+	/// an attempt to edit a file that has already been changed since the edit started
 	case fileVersionMismatch
+	/// failed to insert/update/delete from the database
 	case databaseUpdateFailed
+	/// failed to open connection to Compute Engine
 	case failedToConnectToCompute
+	/// the Compute Engine connection was unexpectedly closed
 	case computeConnectionClosed
+	/// the request , or possibly its arguments, were invalid
 	case invalidRequest
+	/// no user was found with the specified llogin and password
 	case invalidLogin
+	/// user does not have permission
 	case permissionDenied
+	/// an error while encoding to JSON
 	case encoding
+	/// an error while decoding from JSON
 	case decoding
+	/// multiple objects met the requested criteria
 	case duplicate
+	/// the Compute Engine reported an error
 	case compute
 }
 
@@ -32,18 +43,29 @@ public struct DetailedError: Codable {
 	/// details about the error
 	public let details: String
 	
+	/// initialize an error
 	public init(error: SessionError, details: String) {
 		self.error = error
 		self.details = details
 	}
 }
 
-/// Compatible with error codes sent from the compute server
-public enum SessionErrorCode: Int, Codable {
-	case unknown = 0
-	case invalidDirectory = 101
-	case createDirectoryFailed = 102
-	case execInvalidInput = 103
-	case execMarkdownFailed = 104
-	case unknownFile = 105
+extension SessionError: CustomStringConvertible {
+	public var description: String {
+		switch self {
+		case .unknown: return "unknown error"
+		case .fileNotFound: return "file not found"
+		case .fileVersionMismatch: return "file has been changed by another session"
+		case .databaseUpdateFailed: return "failed to update database"
+		case .failedToConnectToCompute: return "failed to open connection to compute engine"
+		case .computeConnectionClosed: return "connection to compute engine unexpectedly closed"
+		case .invalidLogin: return "invalid login or password"
+		case .invalidRequest: return "invalid request"
+		case .permissionDenied: return "permission denied"
+		case .encoding: return "error encoding object"
+		case .decoding: return "error decoding object"
+		case .duplicate: return "that object already exists"
+		case .compute: return "compute error"
+		}
+	}
 }
